586eb74a35c7efb3e31c42dfb3d0e8b16c91ee37
alexspot <alex.kycherenko@gmail.com>
2016-08-01 12:12:14 +0300

    Add heroku deployment workaround

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,9 @@ Bundler.require(*Rails.groups)
 
 module Leather
   class Application < Rails::Application
-
+    config.serve_static_assets = true
+    config.assets.enabled = true
+    config.assets.precompile += ['rails_admin/rails_admin.css', 'rails_admin/rails_admin.js']
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
